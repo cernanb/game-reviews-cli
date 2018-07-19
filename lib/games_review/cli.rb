3eef606896e55f5611b923e783be4798a5bfd7fa
Cernan Bernardo <cernanb@gmail.com>
2018-07-19 15:39:59 -0700

    Complete CLI functionality

--- lib/games_review/cli.rb
+++ lib/games_review/cli.rb
@@ -1,11 +1,40 @@
-class CLI
+class GamesReview::CLI
 
   def start
     puts "Welcome to games reviews"
     puts ""
-    puts "Here are all the games that are reviewed!"
-
+    
     #display a list of the games
-    Scraper.scrape_games
+    GamesReview::Scraper.scrape_games
+    main_menu
+  end
+  
+  def main_menu
+    puts "Here are all the games that are reviewed!"
+    puts ""
+    
+    games = GamesReview::Game.all
+
+    games.each.with_index(1) do |game, index|
+      puts "#{index}. #{game.title}"
+    end
+
+    puts ""
+    puts "Please select a game to view additional details:"
+
+    input = gets.strip
+    index = input.to_i - 1
+
+    game = GamesReview::Game.all[index]
+    if !game.rating || !game.description
+      GamesReview::Scraper.scrape_game_details(game)
+    end
+
+    puts "Here are the details:"
+
+    puts game.rating
+    puts game.description
+
+    main_menu
   end
 end
